d2cbf9f12abb6ae0adf6fc803392410ac72da977
Toby <tobyxdd@gmail.com>
2022-05-19 16:26:16 -0700

    code optimizations

--- bread.js
+++ bread.js
@@ -9,55 +9,56 @@
 minWordLength = 4;
 minTextLength = 50;
 
+function insertTextBefore(text, node, bold) {
+    if (bold) {
+        var span = document.createElement("span");
+        span.style.fontWeight = "bolder";
+        span.appendChild(document.createTextNode(text));
+
+        node.parentNode.insertBefore(span, node);
+    }
+    else {
+        node.parentNode.insertBefore(document.createTextNode(text), node);
+    }
+}
+
 function processNode(node) {
     var walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, {
         acceptNode: (node) => {
-            return (node.nodeValue.trim() !== '') ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+            return (node.nodeValue.trim().length >= minTextLength &&
+                node.parentNode.nodeName !== 'SCRIPT' &&
+                node.parentNode.nodeName !== 'NOSCRIPT' &&
+                node.parentNode.nodeName !== 'STYLE') ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
         }
     });
+
     var node;
     while (node = walker.nextNode()) {
-        if (node.parentNode.nodeName == 'SCRIPT' || node.parentNode.nodeName == 'STYLE') continue;
-
         var text = node.nodeValue;
-        if (text.length < minTextLength) continue;
+        var wStart = -1, wLen = 0, eng = false;
 
-        var wordStart = -1, wordLength = 0, buf = "";
         // English letters only
         for (var i = 0; i <= text.length; i++) { // We use <= here because we want to include the last character in the loop
-            if (i < text.length && text[i].match(/[a-zA-Z]/)) {
-                if (wordStart == -1) {
-                    if (buf.length > 0) {
-                        node.parentNode.insertBefore(document.createTextNode(buf), node);
-                        buf = "";
-                    }
-                    wordStart = i;
+            var cEng = i < text.length ? /[a-zA-Z]/.test(text[i]) : false;
+
+            if (i == text.length || eng !== cEng) {
+                // State flipped or end of string
+                if (eng && wLen >= minWordLength) {
+                    var word = text.substring(wStart, wStart + wLen);
+                    var numBold = Math.ceil(word.length * 0.3);
+                    var bt = word.substring(0, numBold), nt = word.substring(numBold);
+                    insertTextBefore(bt, node, true);
+                    insertTextBefore(nt, node, false);
+                } else if (wLen > 0) {
+                    var word = text.substring(wStart, wStart + wLen);
+                    insertTextBefore(word, node, false);
                 }
-                wordLength++;
+                wStart = i;
+                wLen = 1;
+                eng = cEng;
             } else {
-                if (wordLength > 0) {
-                    if (wordLength >= minWordLength) {
-                        var word = text.substring(wordStart, wordStart + wordLength);
-                        var numBold = Math.ceil(word.length * 0.3);
-                        var bt = word.substring(0, numBold), nt = word.substring(numBold);
-
-                        var span = document.createElement("span");
-                        span.style.fontWeight = "bolder";
-                        span.appendChild(document.createTextNode(bt));
-
-                        node.parentNode.insertBefore(span, node);
-                        node.parentNode.insertBefore(document.createTextNode(nt), node);
-                    } else {
-                        buf += text.substring(wordStart, wordStart + wordLength);
-                    }
-                }
-                wordStart = -1;
-                wordLength = 0;
-                if (i < text.length) buf += text[i];
+                wLen++;
             }
-        }
-        if (buf.length > 0) {
-            node.parentNode.insertBefore(document.createTextNode(buf), node);
         }
 
         node.nodeValue = ""; // Can't remove the node (otherwise the tree walker will break) so just set it to empty
