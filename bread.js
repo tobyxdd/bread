1e8e5c06599c2c161a54b897f328d36a77123637
Toby <tobyxdd@gmail.com>
2022-05-25 10:54:09 -0700

    Update bread.js

--- bread.js
+++ bread.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Bread
-// @match       <all_urls>
-// @version     1.0.1
+// @match       *://*/*
+// @version     1.0.2
 // @author      Toby
 // @description Bread (Bionic Reading) - Read text faster & easier
 // @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
